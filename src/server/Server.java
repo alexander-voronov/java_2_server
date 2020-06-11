00c486e2c703c2b64b52804606192e05d4c39677
Alexander Voronov <dr.strum@gmail.com>
2020-06-11 14:33:11 +0300

    HW_11062020

--- src/server/Server.java
+++ src/server/Server.java
@@ -41,21 +41,63 @@ public class Server {
         }
     }
 
-    public void broadcastMsg(String msg){
-        for (ClientHandler c:clients) {
-            c.sendMsg(msg);
+    public void broadcastMsg(String nick, String msg) {
+        for (ClientHandler c : clients) {
+            c.sendMsg(nick + ": " + msg);
         }
     }
 
-    public void subscribe(ClientHandler clientHandler){
-        clients.add(clientHandler);
+    public void privateMsg(ClientHandler sender, String receiver, String msg) {
+        String message = String.format("[ %s ] private [ %s ] : %s",
+                sender.getNick(), receiver, msg);
+
+        for (ClientHandler c : clients) {
+            if (c.getNick().equals(receiver)) {
+                c.sendMsg(message);
+                if (!sender.getNick().equals(receiver)) {
+                    sender.sendMsg(message);
+                }
+                return;
+            }
+        }
+
+        sender.sendMsg("not found user: " + receiver);
     }
 
-    public void unsubscribe(ClientHandler clientHandler){
+
+    public void subscribe(ClientHandler clientHandler) {
+        clients.add(clientHandler);
+        broadcastClientList();
+    }
+
+    public void unsubscribe(ClientHandler clientHandler) {
         clients.remove(clientHandler);
+        broadcastClientList();
     }
 
     public AuthService getAuthService() {
         return authService;
+    }
+
+    public boolean isLoginAuthorized(String login) {
+        for (ClientHandler c : clients) {
+            if (c.getLogin().equals(login)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void broadcastClientList() {
+        StringBuilder sb = new StringBuilder("/clientlist ");
+
+        for (ClientHandler c : clients) {
+            sb.append(c.getNick()).append(" ");
+        }
+        String msg = sb.toString();
+
+        for (ClientHandler c : clients) {
+            c.sendMsg(msg);
+        }
     }
 }
--- src/server/SimpleAuthService.java
+++ src/server/SimpleAuthService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class SimpleAuthService implements AuthService {
 
-    private class UserData{
+    private class UserData {
         String login;
         String password;
         String nickname;
@@ -22,19 +22,34 @@ public class SimpleAuthService implements AuthService {
     public SimpleAuthService() {
         this.users = new ArrayList<>();
 
-        for (int i = 1; i <= 10 ; i++) {
-            users.add(new UserData("login"+i, "pass"+i, "nick"+i));
+        for (int i = 1; i <= 10; i++) {
+            users.add(new UserData("login" + i, "pass" + i, "nick" + i));
+        }
+
+        for (int i = 1; i <= 3; i++) {
+            users.add(new UserData("" + i, "" + i, "simple_nick" + i));
         }
     }
 
     @Override
     public String getNicknameByLoginAndPassword(String login, String password) {
-        for (UserData o:users ) {
-            if(o.login.equals(login) && o.password.equals(password)){
+        for (UserData o : users) {
+            if (o.login.equals(login) && o.password.equals(password)) {
                 return o.nickname;
             }
         }
 
         return null;
+    }
+
+    @Override
+    public boolean registration(String login, String password, String nickname) {
+        for (UserData o : users) {
+            if (o.login.equals(login)) {
+                return false;
+            }
+        }
+        users.add(new UserData(login, password, nickname));
+        return true;
     }
 }
